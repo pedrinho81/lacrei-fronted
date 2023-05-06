bf2b7bdf7c585fd5e288286e19ff3431bf20018d
Pedro Henrique <1pedrohenri@gmail.com>
2023-05-05 21:50:49 -0300

    fix: redirect fix

--- components/Info/index.tsx
+++ components/Info/index.tsx
@@ -1,41 +1,41 @@
-import Image from "next/image"
-import { InfoProps } from "./info.types"
-import { Container, Button, Text } from "./styles"
-import Link from "next/link"
+import Image from "next/image";
+import { InfoProps } from "./info.types";
+import { Container, Button, Text } from "./styles";
+import Link from "next/link";
+import { useRouter } from "next/router";
 export const Info: React.FC<InfoProps> = ({
   title,
   message,
   buttons,
   img,
-  borderOnLeft
+  borderOnLeft,
 }) => {
+  const router = useRouter();
   return (
     <Container>
       <div>
         <h2>{title}</h2>
         <Text borderOnLeft={borderOnLeft}>{message}</Text>
         <div className="buttons-container">
-          {buttons && buttons.map((button, i) => (
-            <Link href={button.href}>
-              <Button
-                key={i}
-                bg={button.bg}
-                color={button.color}
-                border={button.border}>{button.text}
-              </Button>
-            </Link>
-          )
-          )}
+          {buttons &&
+            buttons.map((button, i) => (
+              <Link href={button.href}>
+                <Button
+                onClick={() => router.push(button.href)}
+                  key={i}
+                  bg={button.bg}
+                  color={button.color}
+                  border={button.border}
+                >
+                  {button.text}
+                </Button>
+              </Link>
+            ))}
         </div>
       </div>
       <div className="image-container">
-        <Image
-          src={img.src}
-          alt={img.alt}
-          width={555}
-          height={421} />
+        <Image src={img.src} alt={img.alt} width={555} height={421} />
       </div>
     </Container>
-  )
-}
-
+  );
+};
--- components/Layout/Header/index.tsx
+++ components/Layout/Header/index.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link"
 import {Container, NavItem} from './styles'
 import { LayoutProps } from "../layout.types";
+import { useRouter } from "next/router";
 
 const Links = [
   {
@@ -20,10 +21,10 @@ const Links = [
 
 
 const Header: React.FC<LayoutProps> = ({currencyHref}) => {
-
+  const router = useRouter()
   return (
     <Container>
-      <Link href={'/'}>Lacrei</Link>
+      <Link onClick={() => router.push('/')} href={'/'}>Lacrei</Link>
       <nav>
         {Links.map((link,i ) => (
           <NavItem key={i} changeColor active={currencyHref === link.href}>
